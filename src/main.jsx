2eed454da55f6fce330759e417a18c89947373aa
1wes <okemwawes@gmail.com>
2024-03-06 16:19:06 +0300

    feat: add root component

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+import ComingSoon from './components/counter.jsx'
+
+import './index.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<ComingSoon/>} ></Route>
+      <Route path='/home' element={<App/>} ></Route>
+    </Routes>
+  </BrowserRouter>,
+)
