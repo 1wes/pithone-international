0567402865559dc5ccfebf3cbd9750febdea089c
1wes <okemwawes@gmail.com>
2024-03-11 13:18:51 +0300

    chore: import and display LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Fragment } from 'react'
+
 import './App.css'
+
+import LandingPage from './components/landing'
 
 function App() {
   return (
     <Fragment>
-      
+      <LandingPage/>
     </Fragment>
   )
 }
