7873d95aac6c57a51707ecc90a2ef1c897b0a560
1wes <okemwawes@gmail.com>
2024-03-12 15:30:26 +0300

    feat: create component that allows input heading as a prop

---
+++ src/components/heading.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import './heading.css';
+
+const SectionHeading = ({heading}) => {
+    
+    return (
+        <Fragment>
+            <h1 className="section-heading">
+                {heading}
+            </h1>
+        </Fragment>
+    )
+}
+export default SectionHeading;
