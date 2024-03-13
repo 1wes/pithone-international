590414ad5ddf8a9c5b176e4fe4260744f340abd2
1wes <okemwawes@gmail.com>
2024-03-13 16:52:36 +0300

    display blank space

---
+++ src/components/extra.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+import './extra.css';
+
+const Extra = () => {
+    
+    return (
+        <Fragment>
+            <main className="section" id="extra-section"> 
+
+            </main>
+        </Fragment>
+    )
+}
+export default Extra;
