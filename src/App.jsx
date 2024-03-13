0a276db1c88e37ca3681a874be18d9c378d272c3
1wes <okemwawes@gmail.com>
2024-03-13 15:08:35 +0300

    chore: add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import './App.css'
 import LandingPage from './components/landing';
 import About from './components/about';
 import Services from './components/services';
+import Contact from './components/contact';
 
 function App() {
   return (
     <Fragment>
       <LandingPage />
       <About />
-      <Services/>
+      <Services />
+      <Contact/>
     </Fragment>
   )
 }
