96ac1d5e871029b27ed54008925cfe4cd211ff4c
1wes <okemwawes@gmail.com>
2024-03-14 12:39:50 +0300

    feat: display image

--- src/components/extra.jsx
+++ src/components/extra.jsx
@@ -2,12 +2,16 @@ import { Fragment } from "react"
 
 import './extra.css';
 
+import clearPithone from '../assets/pithone-bg.png'
+
 const Extra = () => {
     
     return (
         <Fragment>
             <main className="section" id="extra-section"> 
-
+                <div className="centered-content" id="extra-content" >
+                    <img className="clear-image" src={clearPithone} />
+                </div>  
             </main>
         </Fragment>
     )
