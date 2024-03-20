e4964f7ff5f7a265d5151da450ca457e9ed9b326
1wes <okemwawes@gmail.com>
2024-03-20 12:32:55 +0300

    feat(services): add an extra service

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,6 +7,7 @@ import SectionHeading from "./heading";
 import dubai from '../assets/dubai.jpg';
 import boardroom from '../assets/boardroom.png';
 import worker from '../assets/worker.png';
+import passport from '../assets/passport.png';
 
 const Services = () => {
     
@@ -18,19 +19,28 @@ const Services = () => {
                     <div className="services-cards">
                         <ServiceCards image={dubai} service={`International Travels/ Tours Advisory, Planning and Implementation`}
                         description={`Full Tourist
-                        Visa Packages for Individuals, Families and Corporate Organizations. We have set up in a manner that enables us to advise our clients with what best suites their interests. We focus on every nitty-gritty of their course to make sure they enjoy their adventures which they opted us to guide them.
+                        Visa Packages for Individuals, Families and Corporate Organizations: We have set up in a manner that enables 
+                        us to advise our clients with what best suites their interests. We focus on every nitty-gritty of their course 
+                        to make sure they enjoy their adventures which they opted us to guide them.
                         `}/>
                         <ServiceCards image={boardroom} service={`International Education Advisory, Planning and Implementation`}
                         description={`Full Study Visa
                         Packages for Undergraduate, Post-graduate and other Advanced Diploma
-                        Studies Overseas. Our experience in the industry, has seen us make major steps in realizing the dreams of young talents.
+                        Studies Overseas: Our experience in the industry, has seen us make major steps in realizing the dreams of young talents.
                         We have acquired scholarships which are issued on merit to various students in many countries across the world.`}/>
                         <ServiceCards image={worker} service={`International Recruitment Advisory, Planning and Implementation`}
                         description={`Full Work
                         Visa Packages for Young East Africans to Job Opportunities in North America,
-                        Europe and Middle East. As we consider the African growth, we are keen to play a vital role.
+                        Europe and Middle East: As we consider the African growth, we are keen to play a vital role.
                         We partner with both foreign employers and recruitment agencies to secure the best opportunities for job seekers who engage us.
-                        As a result this has seen quality of life improve as well as other opportunities created back at home.`}/>
+                        As a result this has seen quality of life improve as well as other opportunities created back at home.`} />
+                        <ServiceCards image={passport} service={`Kenyan Government Services`}
+                            description={`We offer All Kenyan government services; Immigration(Passport and the like), DCI(Good Conduct), Ministry of 
+                         Foreign Affairs(Attestation), Ministry of Lands(Rates & Boma Yangu), Asylum Seeking Documentation et.al. The offered services
+                         are done legitimately in line with government procedures and at an extra cost, they are hastened. For example, a passport application
+                          will be done as you are advised on which is the most suitable options in terms of the available booklets at the printing department
+                          in the immigration offices. After the biometrics are captured at the desired station, the passport will take 5 working days unlike
+                          the precedented time which is unknown.`} />        
                     </div>
                 </div>
             </main>
