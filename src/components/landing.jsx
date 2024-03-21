b690a2fe6bfc40b4b90d46a9d9d332fb5ccfe610
1wes <okemwawes@gmail.com>
2024-03-21 11:42:26 +0300

    refactor(landing): reduce spacing from commas

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -19,8 +19,8 @@ const LandingPage = () => {
                         Embark on a <span className="important-text">Global</span> Journey.
                     </h1>
                     <p className="hero-tagline">
-                        We Are your Gateway to Exceptional <span className="important-text">Travel/Tours</span> , <span className="important-text">
-                            Education</span> , and <span className="important-text"> Career</span> Opportunities.
+                        We Are your Gateway to Exceptional <span className="important-text">Travel/Tours</span>, <span className="important-text">
+                            Education</span>, and <span className="important-text"> Career</span> Opportunities.
                     </p>
                 </div>
                 <div className="centered-content" id="hero-cta">
