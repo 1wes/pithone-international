7f3454871f0baa41a0e2e7e3b412800d96055d78
1wes <okemwawes@gmail.com>
2026-01-20 23:06:20 +0300

    feat(terms): add terms and conditions and refund policies pages

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,32 +1,32 @@
 import { Fragment } from "react";
-
-import './footer.css';
-
+import "./footer.css";
 import { MdCopyright } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
 
-    const year=new Date().getFullYear()
-    
-    return (
-        <Fragment>
-            <main className="section" id="footer-section">
-                <div className="centered-content">
-                    <div className="footer">
-                        <i>
-                            <MdCopyright/>
-                        </i>
-                        <span className="copyright-year">
-                            {year}
-                        </span>
-                        <span>
-                            Pithone International. All Rights Reserved.
-                        </span>
-                    </div>
-                </div>
-            </main>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <main className="section" id="footer-section">
+        <div className="centered-content">
+          <div className="footer">
+            <div className="footer-left">
+              <i>
+                <MdCopyright />
+              </i>
+              <span className="copyright-year">{year}</span>
+              <span>Pithone International. All Rights Reserved.</span>
+            </div>
+            <div className="footer-links">
+              <Link to="/terms">Terms & Conditions</Link> |
+              <Link to="/refund-policy">Refund Policy</Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Fragment>
+  );
+};
 
 export default Footer;
